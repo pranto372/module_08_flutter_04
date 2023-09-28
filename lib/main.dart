3a9459af4cb7a860f9475bf733d6b88f189df849
pranto372 <prantodeb62@gmail.com>
2023-09-28 11:22:09 +0600

    Stack Position

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:module_08_flutter_04/todo.dart';
 
 void main(){
   runApp(MyApp());
@@ -10,12 +9,88 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "To Do",
-      theme: ThemeData(
-        primarySwatch: Colors.green
+    return const MaterialApp(
+      title: "Counter App",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  const MyHomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomePageUI();
+  }
+}
+
+class MyHomePageUI extends State<MyHomePage>{
+
+  int CountNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stack"),
       ),
-      home: ToDoPage(),
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.green,
+            ),
+            Positioned(
+              top: 0,
+                right: 0,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.blue,
+                ),
+            ),
+            Positioned(
+              bottom: 0,
+                right: 0,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.red,
+                ),
+            ),
+            Positioned(
+              top: 0,
+                left: 0,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.amber,
+                )
+            ),
+            Positioned(
+                bottom: 0,
+                left: 0,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.black,
+                )
+            ),
+            Positioned(
+                top: 50,
+                left: 50,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.white,
+                )
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
