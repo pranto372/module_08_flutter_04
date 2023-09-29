f9e24acb3f81b1c9636b781532c6f46b6dfffbf2
pranto372 <prantodeb62@gmail.com>
2023-09-29 23:26:16 +0600

    module 08 Live test

--- lib/main.dart
+++ lib/main.dart
@@ -1,145 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main(){
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight
-  ]);
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "App",
-      home: MyHomePage(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Image Feed'),
+        ),
+        body: const ImageFeed(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget{
-  const MyHomePage({super.key});
+class ImageFeed extends StatefulWidget {
+  const ImageFeed({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return MyHomePageUI();
-  }
+  ImageFeedState createState() => ImageFeedState();
 }
 
-class MyHomePageUI extends State<MyHomePage>{
+class ImageFeedState extends State<ImageFeed> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile"),
+    final orientation = MediaQuery.of(context).orientation;
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: CircleAvatar(
-                  radius: 200,
-                  backgroundImage: NetworkImage(
-                      "https://media.istockphoto.com/id/1200548447/photo/young-man-taking-a-picture-with-a-dslr-camera-front-shot-at-the-beach-with-copy-space.jpg?s=170667a&w=0&k=20&c=deZRE5yeuCJsCC2Heg5_AlaFyMlqsUbqH9ItuUVqj3I="),
-                ),
-              ),
-            ),
-            Center(
-                child: Text(
-                  "John Doe",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                )
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type."),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      )
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Card(
+          child: Image.network('https://via.placeholder.com/150'),
+        );
+      },
     );
   }
 }
-
