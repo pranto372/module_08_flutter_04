79f70db7a166c5a9f6515952886fbdf023c8db7b
pranto372 <prantodeb62@gmail.com>
2023-09-30 22:11:08 +0600

    module 08 Assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,145 +1,110 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main(){
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight
-  ]);
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "App",
-      home: MyHomePage(),
+      home: Scaffold(
+        body: Homepage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget{
-  const MyHomePage({super.key});
-
-  @override
-  State<StatefulWidget> createState() {
-    return MyHomePageUI();
-  }
-}
-
-class MyHomePageUI extends State<MyHomePage>{
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: CircleAvatar(
-                  radius: 200,
-                  backgroundImage: NetworkImage(
-                      "https://media.istockphoto.com/id/1200548447/photo/young-man-taking-a-picture-with-a-dslr-camera-front-shot-at-the-beach-with-copy-space.jpg?s=170667a&w=0&k=20&c=deZRE5yeuCJsCC2Heg5_AlaFyMlqsUbqH9ItuUVqj3I="),
-                ),
-              ),
-            ),
-            Center(
-                child: Text(
-                  "John Doe",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                )
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type."),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: Container(
-                          height: 130,
-                          width: 140,
-                          child: Image.network("https://images.pexels.com/photos/225157/pexels-photo-225157.jpeg?cs=srgb&dl=pexels-fox-225157.jpg&fm=jpg"),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text("Profile"),
         ),
-      )
+        body: SingleChildScrollView(
+            child: orientation == Orientation.portrait
+                ? const Column(
+              children: [ProfilePic(), PhotoGraphy()],
+            )
+                : const Row(
+              children: [ProfilePic(), PhotoGraphy()],
+            )));
+  }
+}
+
+class PhotoGraphy extends StatelessWidget {
+  const PhotoGraphy({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: orientation == Orientation.portrait
+          ? size.width / 1.2
+          : size.width / 1.7,
+      child: Column(
+        children: [
+          const Center(
+              child: Text(
+                "John Doe",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              )),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+            ),
+          ),
+          SizedBox(
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing:
+                10.0, // Adjust this value to reduce main axis spacing
+                crossAxisSpacing: 10.0,
+              ),
+              itemCount: 6,
+              itemBuilder: (context, index) {
+                return Image.network(
+                    'https://images.unsplash.com/photo-1509873564860-e4b86950dd4f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWFuJTIwaG9sZGluZyUyMGNhbWVyYXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80');
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
 
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    final size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: CircleAvatar(
+          radius: orientation == Orientation.portrait ? 200 : size.width / 6,
+          backgroundImage: const NetworkImage(
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwLU8Bv0Xrg8zWF8OmdAE31XhRQJlw9rJfWQ&usqp=CAU"),
+        ),
+      ),
+    );
+  }
+}
